49573f7fff6d51bd5cff746aaeae6a8e89ea3248
Museer Ansari <museeransari786@gmail.com>
2025-09-21 06:45:14 +0530

    Reduced time complexity for two sum

--- LeetCode/LeetCode.playground/Pages/1.TwoSum.xcplaygroundpage/Contents.swift
+++ LeetCode/LeetCode.playground/Pages/1.TwoSum.xcplaygroundpage/Contents.swift
@@ -8,17 +8,20 @@
  Output: [0,1]
  Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  */
+// -------------------------------------------------------------------------------
 
+/*
+ Space complexity: O(1)
+ Time complexity: O(n)
+ */
 func twoSumSolution(_ nums: [Int], _ target: Int) -> [Int] {
-    
     var map: [Int: Int] = [:]
-    
     for i in 0..<nums.count {
         let num = nums[i]
         let compliment = target - num
-       
+        // Get index if already been stored in map
         if let index = map[compliment] {
-            return [index, i]
+            return [index, i] // map index and current index
         }
         map[num] = i
     }
@@ -27,39 +30,11 @@ func twoSumSolution(_ nums: [Int], _ target: Int) -> [Int] {
 
 twoSumSolution([2,7,11,15], 9)
 
-func twoSum(_ nums: [Int], target: Int) -> [Int] {
-    
-    var map: [Int:Int] = [:]
-    
-    for (index, num) in nums.enumerated() {
-        map[num] = index
-        let compliment = target - num
-        // if map has compliment then curren index and compliment indec will be the solution
-        if let mapIndex = map[compliment] {
-            return [mapIndex, index]
-        }
-    }
-    return []
-}
-
-twoSum([2, 7, 11, 15], target: 9)
-
+/*
+ Space complexity: O(1)
+ Time complexity: O(n2)
+ */
 func twoSumSolution2(_ nums: [Int], target: Int) -> [Int] {
-    
-    for (index1, num1) in nums.enumerated() {
-        let compliment = target - num1
-        for (index2, num2) in nums.enumerated() {
-            if num2 == compliment {
-                return [index1, index2]
-            }
-        }
-    }
-    return[]
-}
-twoSumSolution2([11, 15, 2, 7], target: 9)
-
-func twoSumSolution3(_ nums: [Int], target: Int) -> [Int] {
-    
     for i in 0..<nums.count {
         let compliment = target - nums[i]
         for j in 1..<nums.count {
@@ -70,4 +45,4 @@ func twoSumSolution3(_ nums: [Int], target: Int) -> [Int] {
     }
     return[]
 }
-twoSumSolution3([2, 7, 11, 15], target: 9)
+twoSumSolution2([2, 7, 11, 15], target: 9)
